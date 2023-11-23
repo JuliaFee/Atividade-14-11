d773ecfa2692763b2c28ace00b68744704e26f52
JuliaFee <julia.silva133@aluno.senai.br>
2023-11-23 09:02:50 -0300

    finish code

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express"; // O express serve para criar o servidor
 import { config } from "dotenv"; // O dotenv serve para ler as variáveis de ambiente
+import rotas from "./routes/index.router.js";
 
 config(); // Carrega as variáveis de ambiente
 
@@ -7,6 +8,7 @@ const port = process.env.PORT || 5000; // Pega a porta do arquivo .env ou usa a 
 
 const app = express(); // Cria o servidor e armazena na variável app
 app.use(express.json()); // Configura o servidor para receber requisições com o formato JSON
+app.use(rotas);
 
 // Configura o servidor para responder a requisições na rota /
 app.get("/", (req, res) => {
