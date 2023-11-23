16ab672210873c9a0d2e714b8b44830769d29c29
JuliaFee <julia.silva133@aluno.senai.br>
2023-11-23 09:32:42 -0300

    feat: finish code

--- src/controller/curso.controller.js
+++ src/controller/curso.controller.js
@@ -1,34 +1,66 @@
+import { CursoList } from "../modules/cursos/cursoList.js";
+import { Curso } from "../modules/cursos/curso.js";
+
+const list = new CursoList()
 export const getAllCursos = (req, res) => {
-    return res.status(200).send({ message: "OK", status:"OK" });
+    const cursos = list.getAllCursos();
+
+    if(!cursos.length){
+        return res.status(404).send({ message: "não há registros de curso", origem:"Not Found" });
+    }
+    return res.status(200).send({ message: "Cursos: ", status:"OK", data: cursos});
 };
 
 export const getCursoById = (req, res) => {
     const { id } = req.params;
-    return res.status(200).send({ message: `ID do curso: ${id}` });
-    
+
+    const curso = list.getCursosById(id);
+
+    if(!curso) {
+        return res.status(404).send({ message: "Not found." });
+    }
+    return res.status(200).send({ message: `Curso ID: ${id}!`, data:curso });
 };
 
 export const createCurso = (req, res) => {
-    const {nome, descricao,vagasLimite} = req.body;
+    const {nome, descricao, vagasLimite} = req.body;
 
     if(!nome || !descricao || !vagasLimite) {
-        return res.status(400).send({ message: "Bad Request" });
+        return res.status(400).send({ message: "Bad Request"});
     }
-    return res.status(201).send({ message2: "Registrado com sucesso", message:`nome: ${nome}, descrição: ${descricao}, vagasLimite: ${vagasLimite}`});
+    
+ const curso = new Curso(nome, descricao, vagasLimite);
+    list.createCursos(curso);
+
+    return res.status(200).send({ message: "OK", data: curso});
 };
 
 export const updateCurso = (req, res) => {
     const { id } = req.params;
-    const {nome, descricao,vagasLimite} = req.body;
+    const {nome, descricao, vagasLimite} = req.body;
 
     if(!nome || !descricao || !vagasLimite) {
         return res.status(400).send({ message: "Bad Request"});
     }
 
-    return res.status(200).send({ message: `editado com sucesso`});
-}
+    const curso = list.updateCursos(id, nome, descricao, vagasLimite);
+
+    if(!curso) {
+        return res.status(404).send({ message: "Not Found"});
+    }
+
+    return res.status(200).send({ message: `id: ${id} editado com sucesso`, data: curso });
+};
 
 export const deleteCurso = (req, res) => {
     const { id } = req.params;
-    return res.status(200).send({ message: `deletado com sucesso` });
+
+    const cursos = list.excludecursos(id);
+
+    if(!cursos) {
+        return res.status(404).send({ message: "Not found" });
+    }
+
+    list.excludecursos(id);
+    return res.status(200).send({ message: `id: ${id} deletado com sucesso`, data: cursos });
 }
